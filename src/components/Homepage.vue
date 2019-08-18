0d0e05862da67bfeac2f873096ea2bdb2e4d0dd0
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2019-08-19 00:29:48 +0300

    Initial color selection improved Added suggestions Separated input from vue-color

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -51,18 +51,52 @@
     <div class="container text-theme">
 
       <div v-if="!colors.length">
-        <chrome-picker v-model="selection" :disable-alpha="true"></chrome-picker>
+        <div class="px-2">
+          <div class="flex flex-wrap -mx-2 py-4 bg-theme-400">
+            <div class="w-full md:w-2/3 px-2">
+              <div class="flex flex-wrap items-center">
+                <p class="text-2xl">Suggestions</p>
+                <span class="ml-4 cursor-pointer hover:text-blue-500" @click="suggestionsRefresh = !suggestionsRefresh">
+                  <i class="fas fa-sync"></i>
+                </span>
+              </div>
+              <div class="flex h-64">
+                <div
+                  class="w-12 flex-grow mr-1 cursor-pointer"
+                  :style="'background-color:rgb(' + suggestion.rgb.join(',') + ';'"
+                  v-for="(suggestion, i) in suggestions"
+                  :key="'suggestion-' + i"
+                  @click="selectedSuggestion = suggestion, focus = 'suggestion'"
+                ></div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/3 px-2">
+              <p class="text-2xl">Select manually</p>
+              <input
+                class="shadow appearance-none text-gray-500 border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline mb-2"
+                type="text"
+                placeholder="hex"
+                v-model="hexinput"
+                @focus="focus = 'input'"
+              >
+              <div @click="focus = 'vue-color'">
+                <chrome-picker v-model="selection" :disable-alpha="true" :disable-fields="true"/>
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <pre>{{ selection.hex }}</pre>
+        <div class="text-center mt-4 bg-theme-400 p-4" v-show="selected">
+          <p class="text-2xl leading-none">
+            Selected: <span class="font-bold">{{ selected }}</span>
+          </p>
+          <p class="text-xs">({{ focus }})</p>
+          <button class="btn btn-blue mt-4" @click="addColor">Generate shades</button>
+        </div>
+      </div>
 
-        <!-- <label for="">paste hex color code (full)</label>
-          <input type="text" v-model="hexinput"> -->
-
-        <button class="btn btn-blue" @click="addColor">Generate</button>
-
-        <!-- <br>
-        <br>
-        <button class="btn btn-red" @click="clear">Clear</button> -->
+      <div v-else>
+        <button class="btn btn-red text-xs" @click="clear">Start again</button>
       </div>
 
       <div v-for="(color, i) in colors" :key="'color-' + i">
@@ -77,6 +111,7 @@
 import { mapGetters, mapActions } from 'vuex'
 import { Chrome } from 'vue-color'
 import ColorShades from '@/components/ColorShades'
+import convert from 'color-convert'
 
 export default {
   components: {
@@ -94,26 +129,82 @@ export default {
       selection: {},
       hexinput: '',
       colors: [],
+      randomizer: {
+        lightnessRange: [45, 55],
+        saturationRange: [20, 65],
+        dupeTolerance: 20, // hue
+      },
+      suggestionsRefresh: true,
+      selectedSuggestion: null,
+      focus: null,
     }
   },
   computed: {
+    selected() {
+      if (this.focus === 'suggestion' && this.selectedSuggestion) {
+        return '#' + convert.rgb.hex(this.selectedSuggestion.rgb)
+      }
+      if (this.focus === 'input' && this.hexinput) {
+        // validate
+        let input = this.hexinput
+        if (input.charAt(0) !== '#') {
+          input = '#' + input
+        }
+        if (false === /^#[0-9A-F]{6}$/i.test(input)) {
+          return 'invalid'
+        }
+        return input
+      }
+      if (this.focus === 'vue-color' && this.selection) {
+        return this.selection.hex
+      }
+      return null
+    },
+    suggestions() {
+      this.suggestionsRefresh
+      let colors = []
+      for (let i = 0; i < 10; i++) {
+        let color = this.randomSuggestion(colors)
+        colors.push(color)
+      }
+      return colors
+    },
     ...mapGetters({
       config: 'config/config',
     })
   },
   methods: {
+    randomSuggestion(colors) {
+      let hue = Math.floor(Math.random() * 360)
+      // check duplicate hue (as simple as possible for now)
+      if (colors.some(c => Math.abs(c.hsl.h - hue) < this.randomizer.dupeTolerance)) {
+        return this.randomSuggestion(colors)
+      }
+      let lr = this.randomizer.lightnessRange[1] - this.randomizer.lightnessRange[0]
+      let lightness = 0
+      if (lr > 0) {
+        lightness = this.randomizer.lightnessRange[0] + Math.floor(Math.random() * lr)
+      }
+      let sr = this.randomizer.saturationRange[1] - this.randomizer.saturationRange[0]
+      let saturation = 0
+      if (sr > 0) {
+        saturation = this.randomizer.saturationRange[0] + Math.floor(Math.random() * sr)
+      }
+      let hsl = { h: hue, s: saturation, l:lightness }
+      return {
+        hsl,
+        rgb: convert.hsl.rgb(Object.values(hsl))
+      }
+    },
     clear() {
       this.colors = []
     },
     addColor() {
-    // if (false === /^#[0-9A-F]{6}$/i.test(this.base)) {
-      //   return
-      // }
-      if (!this.selection.hex) {
+      if (!this.selected || this.selected === 'invalid') {
         return
       }
       // pass hex only
-      this.colors.push(this.selection.hex)
+      this.colors.push(this.selected)
     },
     changeTheme(theme) {
       this['config/changeTheme'](theme)
